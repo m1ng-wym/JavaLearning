638d2bbfc2081e1d16ff85b540a9083d500b0462
m1ng-wym <1545044881@qq.com>
2025-04-14 01:08:59 +0800

    0

---
+++ 代码/Java02HelloWorld.java
@@ -0,0 +1,13 @@
+public class Java02HelloWorld {
+    public static void main(String[] args) {      // 这是一个单行注释
+        System.out.println("Hello World!");       // 输出 Hello World！
+        /* 这是
+        一个
+        多行
+        注释
+         */
+        /**
+         * @Author m1ng
+         */
+    }
+}
